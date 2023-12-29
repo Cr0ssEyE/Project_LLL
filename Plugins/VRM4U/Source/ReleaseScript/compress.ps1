24b9af28dd5a7487adaea367a7ed0bb20a11a72d
Cr0ssEyE <hangman0117@gmail.com>
2023-12-29 19:24:54 +0900

    사용하지 않을 것 같은 플러그인 제거

--- Plugins/VRM4U/Source/ReleaseScript/compress.ps1
+++
@@ -1,7 +0,0 @@
-
-Remove-Item -Recurse ../../../../Plugins/VRM4U/Intermediate
-Remove-Item -Recurse ../../../../Plugins/VRM4U/Binaries/Win64/*.pdb
-
-Compress-Archive -Force -Path ../../../../Plugins -DestinationPath $Args[0]
-
-
--- Plugins/VRM4U/Source/ReleaseScript/delIntermediate.ps1
+++
@@ -1,7 +0,0 @@
-
-Remove-Item -Recurse ../../../../Intermediate
-Remove-Item -Recurse ../../../../Binaries
-Remove-Item -Recurse ../../../../Plugins/VRM4U/Intermediate
-Remove-Item -Recurse ../../../../Plugins/VRM4U/Binaries
-
-
