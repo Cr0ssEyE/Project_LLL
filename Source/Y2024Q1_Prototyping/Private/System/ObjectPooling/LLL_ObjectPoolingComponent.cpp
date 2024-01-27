2b1b2ce2649c4724048101ef6fb2277bb5e98e0d
Chrivent <yechandubai@gmail.com>
2024-01-28 04:04:05 +0900

    [#25] 오브젝트 풀링 컴포넌트 생성

---
+++ Source/Y2024Q1_Prototyping/Private/System/ObjectPooling/LLL_ObjectPoolingComponent.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/ObjectPooling/LLL_ObjectPoolingComponent.h"
+
+// Sets default values for this component's properties
+ULLL_ObjectPoolingComponent::ULLL_ObjectPoolingComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULLL_ObjectPoolingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void ULLL_ObjectPoolingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
