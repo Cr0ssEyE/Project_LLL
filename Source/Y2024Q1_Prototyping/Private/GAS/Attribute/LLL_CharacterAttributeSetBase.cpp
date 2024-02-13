eccb73ad443308db7a83a0c1ef268b3bae7dc031
Cr0ssEyE <hangman0117@gmail.com>
2024-02-14 04:59:08 +0900

    [#35] 어트리뷰트 적용 테스트용 함수 작성

--- Source/Y2024Q1_Prototyping/Private/GAS/Attribute/LLL_CharacterAttributeSetBase.cpp
+++ Source/Y2024Q1_Prototyping/Private/GAS/Attribute/LLL_CharacterAttributeSetBase.cpp
@@ -3,7 +3,39 @@
 
 #include "GAS/Attribute/Base/LLL_CharacterAttributeSetBase.h"
 
+#include "GameplayEffectExtension.h"
+
 ULLL_CharacterAttributeSetBase::ULLL_CharacterAttributeSetBase()
 {
 	
+}
+
+bool ULLL_CharacterAttributeSetBase::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data)
+{
+	if(Data.EvaluatedData.Attribute == GetReceiveDamageAttribute())
+	{
+		if(Data.EvaluatedData.Magnitude < 0.f)
+		{
+			Data.EvaluatedData.Magnitude = 0.f;
+		}
+	}
+	return Super::PreGameplayEffectExecute(Data);
+}
+
+void ULLL_CharacterAttributeSetBase::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if(Data.EvaluatedData.Attribute == GetReceiveDamageAttribute())
+	{
+		if(GetCurrentShield() > 0.f)
+		{
+			SetCurrentShield(FMath::Clamp(GetCurrentShield() - GetReceiveDamage(), 0.f, GetMaxShield()));
+		}
+		else
+		{
+			SetCurrentHealth(FMath::Clamp(GetCurrentHealth() - GetReceiveDamage(), 0.f, GetMaxHealth()));
+		}
+		SetReceiveDamage(0.f);
+	}
 }
