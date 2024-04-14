4296add7897736264127797e031f06d66bb2430d
Chrivent <yechandubai@gmail.com>
2024-04-14 16:24:02 +0900

    [#95] 콜리젼 설정 로직 추가

--- Source/Project_LLL/Private/GAS/Ability/Monster/MeleeMonster/SwordDash/LLL_MGA_Dash.cpp
+++ Source/Project_LLL/Private/GAS/Ability/Monster/MeleeMonster/SwordDash/LLL_MGA_Dash.cpp
@@ -3,7 +3,10 @@
 
 #include "GAS/Ability/Monster/MeleeMonster/SwordDash/LLL_MGA_Dash.h"
 
+#include "Components/CapsuleComponent.h"
+#include "Constant/LLL_CollisionChannel.h"
 #include "Entity/Character/Monster/Base/LLL_MonsterBase.h"
+#include "GameFramework/PawnMovementComponent.h"
 #include "GAS/Attribute/Character/Monster/LLL_MonsterAttributeSet.h"
 
 void ULLL_MGA_Dash::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
@@ -12,7 +15,22 @@ void ULLL_MGA_Dash::ActivateAbility(const FGameplayAbilitySpecHandle Handle, con
 	
 	ALLL_MonsterBase* Monster = CastChecked<ALLL_MonsterBase>(GetAvatarActorFromActorInfo());
 	const ULLL_MonsterAttributeSet* MonsterAttributeSet = CastChecked<ULLL_MonsterAttributeSet>(Monster->GetAbilitySystemComponent()->GetAttributeSet(ULLL_MonsterAttributeSet::StaticClass()));
+	Monster->GetCapsuleComponent()->SetCollisionProfileName(CP_EVADE);
 	Monster->LaunchCharacter(Monster->GetActorForwardVector() * MonsterAttributeSet->GetAttackDistance() * 10.0f, true, true);
 
+	FTimerHandle HideTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(HideTimerHandle, this, &ULLL_MGA_Dash::EndDash, 0.5f, false);
+}
+
+void ULLL_MGA_Dash::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	const ALLL_MonsterBase* Monster = CastChecked<ALLL_MonsterBase>(GetAvatarActorFromActorInfo());
+	Monster->GetCapsuleComponent()->SetCollisionProfileName(CP_MONSTER);
+	
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void ULLL_MGA_Dash::EndDash()
+{
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
