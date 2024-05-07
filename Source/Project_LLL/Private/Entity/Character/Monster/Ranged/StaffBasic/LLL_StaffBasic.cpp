2b924a95e856fd1a6d3d3bfb3dd4814f45fa08e4
Chrivent <yechandubai@gmail.com>
2024-05-07 20:55:26 +0900

    [#134] 스태프베이직 기초 세팅

--- Source/Project_LLL/Private/Entity/Character/Monster/Ranged/StaffBasic/LLL_StaffBasic.cpp
+++ Source/Project_LLL/Private/Entity/Character/Monster/Ranged/StaffBasic/LLL_StaffBasic.cpp
@@ -4,14 +4,16 @@
 #include "Entity/Character/Monster/Ranged/StaffBasic/LLL_StaffBasic.h"
 
 #include "Constant/LLL_FilePath.h"
+#include "Entity/Character/Monster/Ranged/StaffBasic/LLL_StaffBasicAIController.h"
+#include "GAS/Attribute/Character/Monster/RangedMonster/StaffBasic/LLL_StaffBasicAttributeSet.h"
 #include "Util/LLL_ConstructorHelper.h"
 
 ALLL_StaffBasic::ALLL_StaffBasic()
 {
-	//CharacterAttributeSet = CreateDefaultSubobject<ULLL_StaffBasicAttributeSet>(TEXT("StaffBasicAttributeSet"));
+	CharacterAttributeSet = CreateDefaultSubobject<ULLL_StaffBasicAttributeSet>(TEXT("StaffBasicAttributeSet"));
 	
 	CharacterDataAsset = FLLL_ConstructorHelper::FindAndGetObject<ULLL_StaffBasicDataAsset>(PATH_STAFF_BASIC_DATA, EAssertionLevel::Check);
-	//AIControllerClass = ALLL_StaffBasicAIController::StaticClass();
+	AIControllerClass = ALLL_StaffBasicAIController::StaticClass();
 }
 
 void ALLL_StaffBasic::BeginPlay()
