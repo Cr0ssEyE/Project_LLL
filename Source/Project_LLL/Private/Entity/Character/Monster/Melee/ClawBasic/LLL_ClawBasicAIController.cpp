81c875364621a72f3d68d1dea38450e08ce89d4e
Chrivent <yechandubai@gmail.com>
2024-05-09 16:46:38 +0900

    [#136] 미사용 함수 제거

--- Source/Project_LLL/Private/Entity/Character/Monster/Melee/ClawBasic/LLL_ClawBasicAIController.cpp
+++ Source/Project_LLL/Private/Entity/Character/Monster/Melee/ClawBasic/LLL_ClawBasicAIController.cpp
@@ -3,24 +3,3 @@
 
 #include "Entity/Character/Monster/Melee/ClawBasic/LLL_ClawBasicAIController.h"
 
-
-// Sets default values
-ALLL_ClawBasicAIController::ALLL_ClawBasicAIController()
-{
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-}
-
-// Called when the game starts or when spawned
-void ALLL_ClawBasicAIController::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ALLL_ClawBasicAIController::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
--- Source/Project_LLL/Public/Entity/Character/Monster/Melee/ClawBasic/LLL_ClawBasicAIController.h
+++ Source/Project_LLL/Public/Entity/Character/Monster/Melee/ClawBasic/LLL_ClawBasicAIController.h
@@ -11,15 +11,4 @@ class PROJECT_LLL_API ALLL_ClawBasicAIController : public ALLL_MeleeMonsterAICon
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this actor's properties
-	ALLL_ClawBasicAIController();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 };
