7f7f3b4c2793cacfa758ecac6d8c838b9bf14370
Dev.Rutin <Dev.Rutin@gmail.com>
2024-05-12 16:28:24 +0900

    [#130] 브랜치 내용 병합

---
+++ Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_Deer_PhysicalProof.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Ability/Character/Player/RewardAbilitiesList/Base/LLL_PGA_RewardAbilityBase.h"
+#include "LLL_PGA_Deer_PhysicalProof.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LLL_API ULLL_PGA_Deer_PhysicalProof : public ULLL_PGA_RewardAbilityBase
+{
+	GENERATED_BODY()
+	
+};
