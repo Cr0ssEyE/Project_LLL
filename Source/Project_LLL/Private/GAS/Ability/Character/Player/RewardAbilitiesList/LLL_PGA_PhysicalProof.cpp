ac7aa5fef8fc5ccb3b156ac7f80dae956739915e
Dev.Rutin <Dev.Rutin@gmail.com>
2024-05-26 17:11:30 +0900

    [#130] 디밸롭 pull

--- Source/Project_LLL/Private/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_PhysicalProof.cpp
+++ Source/Project_LLL/Private/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_PhysicalProof.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_Deer_PhysicalProof.h"
+#include "GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_PhysicalProof.h"
 
 #include "Entity/Object/Skill/LLL_Player_Deer_PhysicalProof.h"
 
-void ULLL_PGA_Deer_PhysicalProof::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,	const FGameplayEventData* TriggerEventData)
+void ULLL_PGA_PhysicalProof::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
--- Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_PhysicalProof.h
+++ Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_PhysicalProof.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GAS/Ability/Character/Player/RewardAbilitiesList/Base/LLL_PGA_RewardAbilityBase.h"
-#include "LLL_PGA_Deer_PhysicalProof.generated.h"
+#include "LLL_PGA_PhysicalProof.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class PROJECT_LLL_API ULLL_PGA_Deer_PhysicalProof : public ULLL_PGA_RewardAbilityBase
+class PROJECT_LLL_API ULLL_PGA_PhysicalProof : public ULLL_PGA_RewardAbilityBase
 {
 	GENERATED_BODY()
 
