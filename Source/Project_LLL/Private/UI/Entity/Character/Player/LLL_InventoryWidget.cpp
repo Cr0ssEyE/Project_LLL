930ced9d89837b1093c5192aacf6f61759f926d1
Cr0ssEyE <hangman0117@gmail.com>
2024-05-27 03:29:30 +0900

    [#165] 이누리아 인벤토리 작업 세이브

--- Source/Project_LLL/Private/UI/Entity/Character/Player/LLL_InventoryWidget.cpp
+++ Source/Project_LLL/Private/UI/Entity/Character/Player/LLL_InventoryWidget.cpp
@@ -3,8 +3,25 @@
 
 #include "UI/Entity/Character/Player/LLL_InventoryWidget.h"
 
+#include "GameplayTagContainer.h"
+#include "Components/VerticalBox.h"
+
 void ULLL_InventoryWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+	
+	for (auto Widget : FirstVerticalBox->GetAllChildren())
+	{
+		
+	}
+}
+
+void ULLL_InventoryWidget::SetEruriaInfo(const FAbilityDataTable* AbilityData)
+{
+	
+}
+
+void ULLL_InventoryWidget::SetEruriaImage(UImage* Image, UTextBlock* TextBlock, FGameplayTag AbilityPartTag, const FAbilityDataTable* AbilityData)
+{
 	
 }
