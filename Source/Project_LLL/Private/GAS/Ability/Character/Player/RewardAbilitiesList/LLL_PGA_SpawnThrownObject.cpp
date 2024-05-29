2e42ce669e405f14786e96c4092458380e61b230
Chrivent <yechandubai@gmail.com>
2024-05-29 18:06:22 +0900

    [#138] 학교 백업

---
+++ Source/Project_LLL/Private/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.h"
+
--- Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnAbilityObject.h
+++ Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnAbilityObject.h
@@ -4,16 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "GAS/Ability/Character/Player/RewardAbilitiesList/Base/LLL_PGA_RewardAbilityBase.h"
-#include "LLL_PGA_ThreateningDeath.generated.h"
+#include "LLL_PGA_SpawnAbilityObject.generated.h"
 
+class ALLL_AbilityObject;
 /**
  * 
  */
 UCLASS()
-class PROJECT_LLL_API ULLL_PGA_ThreateningDeath : public ULLL_PGA_RewardAbilityBase
+class PROJECT_LLL_API ULLL_PGA_SpawnAbilityObject : public ULLL_PGA_RewardAbilityBase
 {
 	GENERATED_BODY()
 
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditAnywhere, DisplayName = "스폰 어빌리티 오브젝트 클래스")
+	TSubclassOf<ALLL_AbilityObject> AbilityObjectClass;
 };
