0bd4edcced29da39098a3a1596dbf2ed67166aa1
Chrivent <yechandubai@gmail.com>
2024-05-30 09:16:08 +0900

    [#138] 깃털 클래스 통일

--- Source/Project_LLL/Private/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.cpp
+++ Source/Project_LLL/Private/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.cpp
@@ -3,3 +3,24 @@
 
 #include "GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.h"
 
+#include "Entity/Character/Player/LLL_PlayerBase.h"
+#include "Entity/Object/Thrown/Base/LLL_ThrownObject.h"
+#include "System/ObjectPooling/LLL_ObjectPoolingComponent.h"
+
+void ULLL_PGA_SpawnThrownObject::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	ALLL_PlayerBase* Player = CastChecked<ALLL_PlayerBase>(GetAvatarActorFromActorInfo());
+	ALLL_ThrownObject* ThrownObject = CastChecked<ALLL_ThrownObject>(Player->GetObjectPoolingComponent()->GetActor(ThrownObjectClass));
+	
+	const AActor* Target = bTargetIsInstigator ? CurrentEventData.Instigator : CurrentEventData.TargetData.Data[0]->GetActors()[0].Get();
+	const FRotator Rotator = FRotationMatrix::MakeFromX(Player->GetActorLocation() - Target->GetActorLocation()).Rotator();
+	ThrownObject->SetActorLocationAndRotation(Player->GetActorLocation(), Rotator);
+	ThrownObject->AddActorLocalRotation(FRotator(0.0f, FMath::RandBool() ? 1.0f : -1.0f, 0.0f));
+	ThrownObject->SetAbilityInfo(AbilityData, GetAbilityLevel());
+	// Todo : 속도 정보 데이터화 필요
+	ThrownObject->Throw(Player, Target, 2000);
+
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
--- Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.h
+++ Source/Project_LLL/Public/GAS/Ability/Character/Player/RewardAbilitiesList/LLL_PGA_SpawnThrownObject.h
@@ -6,6 +6,7 @@
 #include "GAS/Ability/Character/Player/RewardAbilitiesList/Base/LLL_PGA_RewardAbilityBase.h"
 #include "LLL_PGA_SpawnThrownObject.generated.h"
 
+class ALLL_ThrownObject;
 /**
  * 
  */
@@ -13,5 +14,13 @@ UCLASS()
 class PROJECT_LLL_API ULLL_PGA_SpawnThrownObject : public ULLL_PGA_RewardAbilityBase
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	
+	UPROPERTY(EditAnywhere, DisplayName = "스폰 투사체 클래스")
+	TSubclassOf<ALLL_ThrownObject> ThrownObjectClass;
+
+	UPROPERTY(EditAnywhere, DisplayName = "Instigator에게 공격")
+	uint8 bTargetIsInstigator : 1;
 };
