3fa5af9d9d404d1dd40c2a6eb774b7bfe51dbef4
Cr0ssEyE <hangman0117@gmail.com>
2024-05-31 02:30:59 +0900

    [#176] 맵 기믹 컴포넌트 분리, 낙사 가능한 벽 소스코드 생성

---
+++ Source/Project_LLL/Private/System/MapGimmick/LLL_FallableWallGimmick.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/MapGimmick/LLL_FallableWallGimmick.h"
+
+#include "Constant/LLL_GameplayTags.h"
+#include "Entity/Character/Monster/Base/LLL_MonsterBase.h"
+
+
+// Sets default values
+ALLL_FallableWallGimmick::ALLL_FallableWallGimmick()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Wall = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FallableWall"));
+	SetRootComponent(Wall);
+}
+
+// Called when the game starts or when spawned
+void ALLL_FallableWallGimmick::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALLL_FallableWallGimmick::NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	ALLL_MonsterBase* Monster = Cast<ALLL_MonsterBase>(Other);
+	if (!IsValid(Monster))
+	{
+		return;
+	}
+
+	if(!Monster->GetAbilitySystemComponent()->GetGameplayTagCount(TAG_GAS_MONSTER_FALLABLE))
+	{
+		return;
+	}
+
+	// 여기에 연출 입력
+}
---
+++ Source/Project_LLL/Public/System/MapGimmick/LLL_FallableWallGimmick.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "System/Base/LLL_SystemBase.h"
+#include "LLL_FallableWallGimmick.generated.h"
+
+UCLASS()
+class PROJECT_LLL_API ALLL_FallableWallGimmick : public ALLL_SystemBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ALLL_FallableWallGimmick();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> Wall;
+	
+};
