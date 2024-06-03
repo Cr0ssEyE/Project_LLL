3a2b40aa390572f34e068d3a99fd98231f3044dc
Cr0ssEyE <hangman0117@gmail.com>
2024-06-04 06:41:04 +0900

    [#219] 보스방 입장 연출 구현

---
+++ Source/Project_LLL/Private/System/MapGimmick/Components/LLL_SequencerComponent.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/MapGimmick/Components/LLL_SequencerComponent.h"
+
+#include "LevelSequenceActor.h"
+#include "MovieSceneSequencePlaybackSettings.h"
+#include "Entity/Character/Player/LLL_PlayerBase.h"
+#include "Entity/Character/Player/LLL_PlayerController.h"
+#include "Entity/Character/Player/LLL_PlayerUIManager.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values for this component's properties
+ULLL_SequencerComponent::ULLL_SequencerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void ULLL_SequencerComponent::PlayIntroSequence()
+{
+	if (!IsValid(GetWorld()) || !IntroSequencer)
+	{
+		return;
+	}
+	
+	ALevelSequenceActor* IntroSequenceActor = GetWorld()->SpawnActorDeferred<ALevelSequenceActor>(ALevelSequenceActor::StaticClass(), FTransform::Identity);
+	FMovieSceneSequencePlaybackSettings Settings;
+	Settings.bAutoPlay = true;
+	Settings.bHideHud = false;
+	Settings.FinishCompletionStateOverride = EMovieSceneCompletionModeOverride::ForceRestoreState;
+	
+	if (!IsValid(IntroSequenceActor))
+	{
+		return;
+	}
+
+	IntroSequenceActor->PlaybackSettings = Settings;
+	IntroSequenceActor->SetSequence(IntroSequencer);
+	IntroSequenceActor->InitializePlayer();
+	IntroSequenceActor->OnEndPlay.AddDynamic(this, &ULLL_SequencerComponent::OnSequencerEndedCallBack);
+	
+	ALLL_PlayerBase* PlayerCharacter = CastChecked<ALLL_PlayerBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	PlayerCharacter->SetHidden(true);
+	PlayerCharacter->GetPlayerUIManager()->SetAllWidgetVisibility(false);
+	Cast<ALLL_PlayerController>(PlayerCharacter->GetController())->SetUIInputMode();
+	
+	IntroSequenceActor->FinishSpawning(FTransform::Identity);
+}
+
+void ULLL_SequencerComponent::OnSequencerEndedCallBack(AActor* Actor, EEndPlayReason::Type EndPlayReason)
+{
+	ALLL_PlayerBase* PlayerCharacter = CastChecked<ALLL_PlayerBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	PlayerCharacter->SetHidden(false);
+	PlayerCharacter->GetPlayerUIManager()->SetAllWidgetVisibility(true);
+	Cast<ALLL_PlayerController>(PlayerCharacter->GetController())->SetGameInputMode();
+	
+	if (bLoadToOtherLevel)
+	{
+		UGameplayStatics::OpenLevelBySoftObjectPtr(GetWorld(), LoadLevel);
+	}
+}
+
+
+
---
+++ Source/Project_LLL/Public/System/MapGimmick/Components/LLL_SequencerComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "LLL_SequencerComponent.generated.h"
+
+
+class ULevelSequence;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECT_LLL_API ULLL_SequencerComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	ULLL_SequencerComponent();
+
+	void PlayIntroSequence();
+
+protected:
+	UFUNCTION()
+	void OnSequencerEndedCallBack(AActor* Actor, EEndPlayReason::Type EndPlayReason);
+	
+protected:
+	UPROPERTY(EditAnywhere, DisplayName = "입장 인트로 재생 시퀀서")
+	TObjectPtr<ULevelSequence> IntroSequencer;
+
+	UPROPERTY(EditAnywhere, DisplayName = "레벨 이동 여부")
+	uint8 bLoadToOtherLevel : 1;
+
+	UPROPERTY(EditAnywhere, DisplayName = "레벨")
+	TObjectPtr<UWorld> LoadLevel;
+	
+};
