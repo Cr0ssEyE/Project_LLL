d1a26bfa8c2b0b355e9f814b4c9e6397ae439a63
Jung Jaeha <wjdwogk5218@gmail.com>
2024-09-25 13:09:14 +0900

    그래픽브랜치 병합

---
+++ Source/Project_LLL/Public/GAS/Ability/Character/Player/LLL_PGA_Skill.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LLL_PlayerGameplayAbilityBase.h"
+#include "LLL_PGA_Skill.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LLL_API ULLL_PGA_Skill : public ULLL_PlayerGameplayAbilityBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	UFUNCTION()
+	void SkillCompleteCallBack(FGameplayEventData Payload);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	TObjectPtr<UAnimMontage> SkillMontage;
+
+	uint8 bSkillComplete : 1;
+};
