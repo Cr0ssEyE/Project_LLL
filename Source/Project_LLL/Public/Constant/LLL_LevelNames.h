de6690a4033164c84e1d68225c31fb6f98019491
Dev.Rutin <Dev.Rutin@gmail.com>
2024-10-29 08:49:03 +0900

    [#155] 중간시연 빌드
    플레이어 애니메이션에 문제 있을 시 경환님께 말씀드려서 해결 필요

--- Source/Project_LLL/Public/Constant/LLL_LevelNames.h
+++ Source/Project_LLL/Public/Constant/LLL_LevelNames.h
@@ -13,6 +13,7 @@
 #define LEVEL_STAGE1_03 TEXT("LV_Stage1_LD03")
 #define LEVEL_STAGE1_04 TEXT("LV_Stage1_LD04")
 #define LEVEL_STAGE1_05 TEXT("LV_Stage1_LD05")
+#define LEVEL_STAGE1_BOSS TEXT("LV_Stage1_Boss")
 
 #define LEVEL_TUTORIAL TEXT("LV_Tutorial")
 #define LEVEL_TEST TEXT("MapCreateLevel")
