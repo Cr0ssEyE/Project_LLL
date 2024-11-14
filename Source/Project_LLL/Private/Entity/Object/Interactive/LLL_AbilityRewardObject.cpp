35aaf20f8b886b93e9234636695860969c5979d6
Dev.Rutin <Dev.Rutin@gmail.com>
2024-11-15 07:43:01 +0900

    [#155] 보상 오브젝트 수정 및 획득 이펙트 적용

--- Source/Project_LLL/Private/Entity/Object/Interactive/LLL_AbilityRewardObject.cpp
+++ Source/Project_LLL/Private/Entity/Object/Interactive/LLL_AbilityRewardObject.cpp
@@ -3,10 +3,12 @@
 
 #include "Entity/Object/Interactive/LLL_AbilityRewardObject.h"
 
+#include "NiagaraComponent.h"
 #include "Entity/Character/Player/LLL_PlayerBase.h"
 #include "Entity/Character/Player/LLL_PlayerUIManager.h"
 #include "Game/LLL_GameInstance.h"
 #include "Game/LLL_RewardGimmickSubsystem.h"
+#include "Kismet/GameplayStatics.h"
 #include "UI/System/LLL_SelectRewardWidget.h"
 
 void ALLL_AbilityRewardObject::SetInformation(const FRewardDataTable* Data, const uint32 Index)
@@ -20,8 +22,20 @@ void ALLL_AbilityRewardObject::InteractiveEvent(AActor* InteractedActor)
 	Super::InteractiveEvent(InteractedActor);
 	
 	//AbilityRewardInteractionDelegate->Broadcast(AbilityCategory);
+	NiagaraComponents[0]->OnSystemFinished.AddDynamic(this, &ALLL_AbilityRewardObject::ActiveUI);
+	NiagaraComponents[0]->ActivateSystem();
 	
-	const ALLL_PlayerBase* Player = CastChecked<ALLL_PlayerBase>(InteractedActor);
+}
+
+void ALLL_AbilityRewardObject::SetAbilityCategory(EAbilityCategory Category)
+{
+	AbilityCategory = Category;
+}
+
+
+void ALLL_AbilityRewardObject::ActiveUI(UNiagaraComponent* PSystem)
+{
+	const ALLL_PlayerBase* Player = CastChecked<ALLL_PlayerBase>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
 	
 	ULLL_SelectRewardWidget* SelectRewardWidget = Player->GetPlayerUIManager()->GetSelectRewardWidget();
 
@@ -35,9 +49,6 @@ void ALLL_AbilityRewardObject::InteractiveEvent(AActor* InteractedActor)
 	SelectRewardWidget->SetVisibility(ESlateVisibility::Visible);
 	SelectRewardWidget->SetIsEnabled(true);
 	SelectRewardWidget->FocusToUI();
-}
 
-void ALLL_AbilityRewardObject::SetAbilityCategory(EAbilityCategory Category)
-{
-	AbilityCategory = Category;
+	Destroy();
 }
