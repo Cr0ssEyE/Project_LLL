46f8c2060a710a8358fad00e297ab85a990a10f4
Chrivent <yechandubai@gmail.com>
2024-11-14 18:04:48 +0900

    [#155] 차지 공격 낙사 판정 구현

---
+++ Source/Project_LLL/Public/Game/LLL_FallOutSubsytem.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LLL_FallOutSubsytem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_LLL_API ULLL_FallOutSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	void FallOutBegin(AActor* Actor, FVector HitNormal, FVector HitLocation);
+	void FallOutStart(AActor* Actor, FVector HitNormal);
+};
