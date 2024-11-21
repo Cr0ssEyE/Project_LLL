a183206d5fd660837939a41cebd5306085cc9fb2
Chrivent <yechandubai@gmail.com>
2024-11-21 22:30:02 +0900

    [#155] dps 테스터 예외 처리

--- Source/Project_LLL/Private/Entity/Character/Monster/Base/LLL_MonsterBase.cpp
+++ Source/Project_LLL/Private/Entity/Character/Monster/Base/LLL_MonsterBase.cpp
@@ -159,7 +159,7 @@ void ALLL_MonsterBase::Tick(float DeltaSeconds)
 			IsMoving = CastChecked<ALLL_MonsterBaseAIController>(GetController())->GetPathFollowingComponent()->GetStatus() == EPathFollowingStatus::Moving;
 		}
 		
-		if ((VelocityWithKnockBack == FVector::ZeroVector || IsMoving) && bStartKnockBackVelocity && !CharacterAnimInstance->Montage_IsPlaying(MonsterBaseDataAsset->DamagedAnimMontage))
+		if ((VelocityWithKnockBack == FVector::ZeroVector || IsMoving) && bStartKnockBackVelocity && (Cast<ALLL_DPSTester>(this) || !CharacterAnimInstance->Montage_IsPlaying(MonsterBaseDataAsset->DamagedAnimMontage)))
 		{
 			CustomTimeDilation = 1.f;
 			UE_LOG(LogTemp, Log, TEXT("%s가 넉백 끝"), *GetName())
